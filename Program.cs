a31f236a3e8ce7925bee5d9a478c93d6de1aecd4
A252460 <A252460@LSBSA-416QCS3>
2024-01-13 09:28:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pascal_triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of rows: ");
+            int rows = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rows; i++)
+            {
+                // Print leading spaces for alignment
+                for (int j = 0; j < rows - i - 1; j++)
+                {
+                    Console.Write("  ");
+                }
+
+                // Calculate and print binomial coefficients
+                int coeff = 1;
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("{0}  ", coeff);
+                    coeff = coeff * (i - j) / (j + 1);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Pascal_triangle")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Standard Bank of South Africa")]
+[assembly: AssemblyProduct("Pascal_triangle")]
+[assembly: AssemblyCopyright("Copyright © Standard Bank of South Africa 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88580245-4ae1-452c-a605-c79363505e1a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
